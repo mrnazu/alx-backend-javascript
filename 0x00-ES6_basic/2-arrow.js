149624145d5d174ec3ddc74e8651be1951bb2e56
mrnazu <samuelamsalu1998@gmail.com>
2023-11-27 21:38:22 +0300

    2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,13 @@
+// Define a function to get a list of neighborhoods
 export default function getNeighborhoodsList() {
+  // Initialize an array with some San Francisco neighborhoods
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  // Define an arrow function to add a new neighborhood
   this.addNeighborhood = (newNeighborhood) => {
+    // Push the new neighborhood to the array
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    // Return the updated list of neighborhoods
     return this.sanFranciscoNeighborhoods;
   };
 }
