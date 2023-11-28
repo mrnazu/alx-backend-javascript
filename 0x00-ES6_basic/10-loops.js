5c5437ac3fd977bf1508ca55d3b778c78ed3ab59
mrnazu <samuelamsalu1998@gmail.com>
2023-11-28 10:19:31 +0300

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
-  }
-
-  return array;
+export default function appendToEachArrayValue(array, appendString){
+	const arrayEnd = [];
+	for (const idx of array) {
+		arrayEnd.push(`${appendString}${idx}`);
+	}
+	return arrayEnd;
 }
+
 
