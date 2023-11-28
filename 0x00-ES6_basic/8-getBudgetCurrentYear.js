e7fd752b779e8b0892c4b4641b3b902fe6506222
mrnazu <samuelamsalu1998@gmail.com>
2023-11-28 09:15:01 +0300

    8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,9 +4,7 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  return{
+  return {
 	  [`income-${getCurrentYear()}`]: income,
 	  [`gdp-${getCurrentYear()}`]: gdp,
 	  [`capita-${getCurrentYear()}`]: capita,
