351663e1509bce3bb8831ec20bc7ea22fabf9e09
mrnazu <samuelamsalu1998@gmail.com>
2023-11-29 12:03:13 +0300

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success){
 	new Promise(function(resolve, reject){
 		if(true){
-			resolve({
-				"status": 200,
-				"body": "Success"
+			resolve({ 
+				"status": 200, 
+				"body": "Success",
 			})else{
-				reject('The fake API is not working currently')
+				reject(new Error('The fake API is not working currently'))
 			}
 		}
 	})
