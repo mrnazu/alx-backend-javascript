4caa8ba56d8cd2f5829e2054a1957f1fdf384b5e
mrnazu <samuelamsalu1998@gmail.com>
2023-12-06 10:22:54 +0300

    Write an interface named Student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,6 +1,22 @@
-const Student: {
-	firstname: String,
-	lastname: String,
-	age: Number,
-	location: String
+interface Student: {
+	firstname: string;
+	lastname: string;
+	age: number;
+	location: string;
 }
+
+const student1: Student = {
+	firstname: "Samuel",
+	lastname: "Amsalu",
+	age: 18,
+	location: "Oromia"
+};
+
+const studnet2: Student = {
+	firstname: "Jane",
+	lastname: "Smith",
+	age: 22,
+	location: "Oromia"
+};
+
+const studentsList[]: Student[] = [student1, student2];
